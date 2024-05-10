447a488baba7c8d6fd46e30207b3bd6bf89a378e
Rubenclaassen1 <ruben.claassen@home.nl>
2024-05-10 15:34:57 +0200

    Test project
    OpenCVSetup is a test

---
+++ OpenCVSetup/OpenCVSetup.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+#ifdef _DEBUG
+#pragma comment ( lib, "opencv_world490d")
+#else
+#pragma comment( lib, "opencv_world490" )
+#endif
+
+
+
+/////////////////  Images  //////////////////////
+
+//void main() {
+//
+//	string path = "Resources/test.png";
+//	Mat img = imread(path);
+//	imshow("Image", img);
+//	waitKey(0);
+//
+//}
+
+
+///////////////  Video  //////////////////////
+
+void main() {
+
+	string path = "Resources/test_video.mp4";
+	VideoCapture cap(path);
+	Mat img;
+
+	while (true) {
+
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(20);
+	}
+}
+
+
+/////////////////  Webcam  //////////////////////
+
+//void main() {
+//
+//	VideoCapture cap(0);
+//	Mat img;
+//
+//	while (true) {
+//
+//		cap.read(img);
+//		imshow("Image", img);
+//		waitKey(1);
+//	}
+//}
