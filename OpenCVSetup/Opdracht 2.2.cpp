35179fa392f75ef76ba4d58bc5c1b28e80b32baa
Double <Lucvds2001@Gmail.com>
2024-05-28 14:35:50 +0200

    2.3 finished with track bars

--- OpenCVSetup/Opdracht 2.2.cpp
+++ OpenCVSetup/Opdracht 2.2.cpp
@@ -19,15 +19,36 @@ using namespace std;
 void main() {
 
 	string path = "Resources/Johannusthemannus.png";
+	string hogpath = "hog rider.png";
+	Mat hog = imread(hogpath);
 	Mat img = imread(path);
-	Mat canny;
-	Mat blur;
+	Mat canny, hogcanny;
+
+	int maxthreshold = 0;
+	int maxthreshold2 = 0;
+	int minthreshold = 0;
+	int minthreshold2 = 0;
+
+	namedWindow("Trackbars", (640, 200));
+	createTrackbar("Threshold Min", "Trackbars", &minthreshold, 1000);
+	createTrackbar("Threshold Max", "Trackbars", &maxthreshold, 1000);
+	namedWindow("Trackbars2", (640, 200));
+	createTrackbar("Threshold Min", "Trackbars2", &minthreshold2, 1000);
+	createTrackbar("Threshold Max", "Trackbars2", &maxthreshold2, 1000);
+
+	while (true)
+	{
+		//Changes the color space to Gray for a black and white look
+		Canny(img, canny, maxthreshold, minthreshold);
+		Canny(hog, hogcanny, maxthreshold2, minthreshold2);
+
+		imshow("default", img);
+		imshow("default hog", hog);
+		imshow("hog", hogcanny);
+		imshow("canny", canny);
+		waitKey(1);
+	}
+
 	
 
-	//Changes the color space to Gray for a black and white look
-	Canny(img, canny, 200, 200);
-
-	imshow("default", img);
-	imshow("canny", canny);
-	waitKey(0);
 }
--- OpenCVSetup/Opdracht 2.3.cpp
+++ OpenCVSetup/Opdracht 2.3.cpp
@@ -14,22 +14,40 @@ using namespace std;
 
 
 
-/////////////////  Ilate / erode  //////////////////////
+///////////////// dilate / erode  //////////////////////
+
+//void main() {
+//
+//	string path = "Resources/Johannusthemannus.png";
+//	Mat img = imread(path), cannyimg,  dilateimg, erodeimg;
+//
+//	Canny(img, cannyimg, 150, 200);
+//
+//	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+//
+//	//Changes the color space to Gray for a black and white look
+//	dilate(cannyimg, dilateimg, kernel);
+//	erode(dilateimg, erodeimg, kernel);
+//
+//	imshow("canny", cannyimg);
+//	imshow("default", img);
+//	imshow("dilate", dilateimg);
+//	imshow("erode", erodeimg);
+//	waitKey(0);
+//}
+
+/////////////////  removing small lines  //////////////////////
 
 void main() {
 
-	string path = "Resources/Johannusthemannus.png";
-	Mat img = imread(path), cannyimg,  dilateimg, erodeimg;
+	string path = "Resources/kleurenpatroon1.jpg";
+	Mat img = imread(path), dilateimg, erodeimg;
 
-	Canny(img, cannyimg, 150, 200);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(7, 7));
 
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
-
-	//Changes the color space to Gray for a black and white look
-	dilate(cannyimg, dilateimg, kernel);
+	dilate(img, dilateimg, kernel);
 	erode(dilateimg, erodeimg, kernel);
 
-	imshow("canny", cannyimg);
 	imshow("default", img);
 	imshow("dilate", dilateimg);
 	imshow("erode", erodeimg);
